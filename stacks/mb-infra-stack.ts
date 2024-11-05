81d5a3f0fa2ce3e002a3cbb00965478b1ece5b7e
Pawel Rugala <webdevpablo@gmail.com>
2024-11-05 08:29:36 +0100

    feat: ecr ecs ssm

--- stacks/mb-infra-stack.ts
+++ stacks/mb-infra-stack.ts
@@ -2,7 +2,11 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { InfraConfig } from "../schemas/infra.config";
 import { Vpc } from "aws-cdk-lib/aws-ec2";
-import { Cluster } from "aws-cdk-lib/aws-ecs";
+import { Cluster, ContainerImage } from "aws-cdk-lib/aws-ecs";
+import { ApplicationLoadBalancer } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Repository, TagMutability } from "aws-cdk-lib/aws-ecr";
+import { SsmSecrets } from "../constructs/ssm.construct";
+import { EcsServiceConstruct } from "../constructs/ecsService.construct";
 
 type MbInfraProps = cdk.StackProps & InfraConfig;
 
@@ -21,34 +25,34 @@ export class MbInfraStack extends cdk.Stack {
             vpc,
         });
 
-        // const lb = new ApplicationLoadBalancer(this, `${props.stage}/alb`, {
-        //     vpc,
-        //     internetFacing: true,
-        // });
+        const lb = new ApplicationLoadBalancer(this, `${stage}/alb`, {
+            vpc,
+            internetFacing: true,
+        });
 
-        // props.apps.forEach((app) => {
-        //     const ecr = new Repository(this, `${props.stage}-${app.name}`, {
-        //         repositoryName: app.name,
-        //         imageTagMutability: TagMutability.IMMUTABLE,
-        //     });
-        //
-        //     const secrets = new SsmSecrets(this, `${props.stage}/${app.name}/Secrets`, {
-        //         secretPath: `/${props.stage}/${app.name}`,
-        //         secretsNames: app.parameterStoreSecrets,
-        //     });
-        //
-        //     new EcsServiceConstruct(this, app.name, {
-        //         serviceName: app.name,
-        //         containerPort: app.port,
-        //         containerImage: ContainerImage.fromAsset(app.dockerImagePath),
-        //         memoryLimit: app.memory,
-        //         cpuLimit: app.cpu,
-        //         secrets: secrets.secrets,
-        //         cluster,
-        //         vpc,
-        //         lb,
-        //         ecr,
-        //     });
-        // });
+        props.apps.forEach((app) => {
+            const ecr = new Repository(this, `${stage}-${app.name}`, {
+                repositoryName: app.name,
+                imageTagMutability: TagMutability.IMMUTABLE,
+            });
+
+            const secrets = new SsmSecrets(this, `${stage}/${app.name}/Secrets`, {
+                secretPath: `/${stage}/${app.name}`,
+                secretsNames: app.parameterStoreSecrets,
+            });
+
+            new EcsServiceConstruct(this, app.name, {
+                serviceName: app.name,
+                containerPort: app.port,
+                containerImage: ContainerImage.fromAsset(app.dockerImagePath),
+                memoryLimit: app.memory,
+                cpuLimit: app.cpu,
+                secrets: secrets.secrets,
+                cluster,
+                vpc,
+                lb,
+                ecr,
+            });
+        });
     }
 }
